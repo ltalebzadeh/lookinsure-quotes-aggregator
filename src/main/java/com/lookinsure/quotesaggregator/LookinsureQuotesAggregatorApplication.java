d2888f2eff176f767feaaa9af3d177523be292e3
leila <l.talebzadeh93@gmail.com>
2026-02-24 22:31:43 +0330

    Initial commit: Project setup with Spring Boot and H2

---
+++ src/main/java/com/lookinsure/quotesaggregator/LookinsureQuotesAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.lookinsure.quotesaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookinsureQuotesAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookinsureQuotesAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lookinsure/quotesaggregator/LookinsureQuotesAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lookinsure.quotesaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookinsureQuotesAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
