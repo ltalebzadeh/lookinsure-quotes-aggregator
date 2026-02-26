0c1cd64a56fc5626b0da8057e8ff9b50e8ece9c2
leila <l.talebzadeh93@gmail.com>
2026-02-26 14:44:58 +0330

    chore: enable DTO-based page serialization
    This resolves the Spring Data warning regarding unstable PageImpl
    serialization by explicitly adding @EnableSpringDataWebSupport
    with serialization mode set to VIA_DTO.

--- src/main/java/com/lookinsure/quotesaggregator/LookinsureQuotesAggregatorApplication.java
+++ src/main/java/com/lookinsure/quotesaggregator/LookinsureQuotesAggregatorApplication.java
@@ -3,9 +3,13 @@ package com.lookinsure.quotesaggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class LookinsureQuotesAggregatorApplication {
 
     public static void main(String[] args) {
